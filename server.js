85f2edb66c92cbda8cce98d4153439c571162680
Mike Luz <onlymikeluz@gmail.com>
2017-06-29 09:31:56 -0400

    first deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var Promise = require('bluebird'); // require bluebird so we can use Promise.all
 
 server.on('request', require('./app')); // when server receives a request, use 'app.js' to determine what to do with it
 
-server.listen(3001, function() { // first arg is port server will listen on, second arg is callback once listening
-	console.log('Server is listening on port 3001!'); // 'I'm listening', i.e., success message
+server.listen((process.env.PORT || 5000), function() { // first arg is port server will listen on, second arg is callback once listening
+	console.log('Server is listening!'); // 'I'm listening', i.e., success message
 });
 
